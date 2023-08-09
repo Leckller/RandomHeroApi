30f3a7904c2d0934c12291dbc7a585623c40ca33
Leckller <7leckler@gmail.com>
2023-08-09 12:00:46 -0300

    feat: random images

--- src/main.js
+++ src/main.js
@@ -1,10 +1,25 @@
 import './style.css'
 
-const base = 'https://akabab.github.io/superhero-api/api';
+const urlBase = 'https://akabab.github.io/superhero-api/api';
+const imgHTML = document.querySelector('img');
+const h2Name = document.querySelector('h2');
+const btn = document.querySelector('button');
 
-function allIds(id) {
-  const rotaAll = '/all.json'
-  return fetch(`${base}${rotaAll}`).then((res) => res.json()).then((res) => console.log(res[id]))
+function randomNumber() {
+  return Math.round(Math.random() * 600);
 }
 
-allIds(5);
+
+btn.addEventListener('click', (e) => {
+  e.preventDefault();
+  const randomId = randomNumber();
+
+  fetch(`${urlBase}/all.json`)
+    .then((response) => response.json())
+    .then((vetor) => {
+      const endImg = vetor[randomId].slug;
+      imgHTML.src = `${urlBase}/images/md/${endImg}.jpg`;
+      h2Name.innerText = vetor[randomId].name;
+    })
+    .catch((error) => window.alert('error! foi gerado um elemento inválido.'))
+})
